79d45e46fd6e78af282905b0bf6bf06f038cda63
Mystic <mystic.rick@gmail.com>
2010-09-29 09:59:57 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RLinkIT::Application.config.secret_token = '547f5cbfdec3ff957fdfc4e4515cb7821c53849a919bacbed7ba68e4b8f2765ccbd455720ac3c32604bcdc8acdbda0745436f9a01f431c0772ef3992fdb08ff4'
